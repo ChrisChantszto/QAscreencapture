7a981ebb0522d81f425fbf0b041df9a7daf01ecc
ChrisChantszto <68599772+ChrisChantszto@users.noreply.github.com>
2021-12-09 15:47:48 +0800

    Create cap.py

---
+++ cap.py
@@ -0,0 +1,27 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = Firefox()
+
+driver.set_window_size(1300, 800)
+
+driver.get(url)
+url = ""
+time.sleep(10)
+driver.save_screenshot("test1.png")
+print("hi")
+
+actions = []
+actions = driver.find_elements(By.CLASS_NAME, "sitemapPageName")
+
+count = 0
+for e in actions:
+    print(e)
+    e.click()
+    time.sleep(5)
+    print("test" + str(count) + ".png")
+    driver.save_screenshot("test" + str(count) + ".png")
+    count += 1
